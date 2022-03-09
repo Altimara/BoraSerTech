09faa11dd67df0c6afefd6ec3cdadb65c71ab322
Altimara Wrobel Carneiro Schmitz <99455185+Altimara@users.noreply.github.com>
2022-03-09 16:23:05 -0300

    Create index.js

---
+++ Tecnicas_Programacao/Aula7_8mar22/dotenv/index.js
@@ -0,0 +1,6 @@
+require('dotenv').config();
+
+console.log(process.env.NOME, process.env.SOBRENOME); //Arquivo /.env ao executar o Index.js irá 
+                                                    //mostrar as informações solicitadaa: 'Altimara Schmitz'
+
+//console.log(process.env.CITY); //Arquivo variável do WINDOWS
