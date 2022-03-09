9e16659a2925d4ea92f43fdb79984236086b40ea
Altimara Wrobel Carneiro Schmitz <99455185+Altimara@users.noreply.github.com>
2022-03-09 17:02:25 -0300

    Dotenv

--- Tecnicas_Programacao/Aula7_8mar22/dotenv/index.js
+++ Tecnicas_Programacao/Aula7_8mar22/dotenv/index.js
@@ -1,6 +1,30 @@
+/*Criar um arquivo .env e adicionar uma variável NOME
+Criar um arquivo .js e imprimir essa variável
+Criar variável de ambiente com export SOBRENOME=Wayne
+Imprimir essa variável SOBRENOME
+Sobrescrever SOBRENOME no dotenv e imprimir valor novo */
+
 require('dotenv').config();
 
 console.log(process.env.NOME, process.env.SOBRENOME); //Arquivo /.env ao executar o Index.js irá 
                                                     //mostrar as informações solicitadaa: 'Altimara Schmitz'
 
 //console.log(process.env.CITY); //Arquivo variável do WINDOWS
+
+
+const axios = require('axios').default;
+//axios.get("https://www.youtube.com/watch?v=d7bKeG9hb-0")
+//    .then(function(resposta){console.log(resposta.data); //retorna apenas os dados da API
+//    });
+//    axios.get("https://www.youtube.com/watch?v=d7bKeG9hb-0")
+//    .then(function(resposta){console.log(resposta.headers); //retorna apenas os headers da API
+//    });
+
+
+//    axios.get("https://www.youtube.com/watch?v=d7bKeG9hb-0")
+//    .then(function(resposta){console.log(resposta.status); //retorna apenas status da conexão do API, ex. 200
+//    });    
+require('dotenv').config();
+const axios = require('axios').default;
+
+axios.get(URL,{'Authorization': process.env.API_KEY});
